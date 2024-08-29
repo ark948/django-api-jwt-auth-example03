69da685f8d445589a2ffac79453ca34c6bdf7d3b
ark948 <armankarimi948@gmail.com>
2024-08-29 22:59:19 +0330

    backend: CustomUser model and manager

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,17 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import environs
+import environ
+from datetime import timedelta
 from pathlib import Path
-env = environs.Env(
+env = environ.Env(
     DEBUG=(bool, False)
-    # default for debug, if not provided
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environs.Env.read_env(BASE_DIR / ".env")
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -132,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
